72d1e3f204c05a7b9c55cdcecfd2bf58b3ebc83e
Niklas Salmoukas <niklas@salmoukas.com>
2017-02-11 07:59:23 +0100

    first draft of build-iso.sh script

--- ubuntu/16.10/build-iso.sh
+++ ubuntu/16.10/build-iso.sh
@@ -20,17 +20,32 @@ esac
 
 TARGET_ISO=${2:-"`pwd`/ubuntu-16.10-$IMAGE_TYPE-amd64-unattended.iso"}
 
-echo "IMAGE_TYPE=$IMAGE_TYPE"
-echo "SOURCE_ISO_URL=$SOURCE_ISO_URL"
-echo "TARGET_ISO=$TARGET_ISO"
-exit 0
-
 # get all directories
 CURRENT_DIR="`pwd`"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMP_DOWNLOAD_DIR="`mktemp -d`"
 TMP_DISC_DIR="`mktemp -d`"
 TMP_INITRD_DIR="`mktemp -d`"
+
+# download and extract source iso
+wget "$SOURCE_ISO_URL" -O "$TMP_DOWNLOAD_DIR/source.iso"
+7z x "$TMP_DOWNLOAD_DIR/source.iso" "-o$TMP_DISC_DIR"
+
+# patch boot menu
+cd "$TMP_DISC_DIR"
+patch -p1 -i "$SCRIPT_DIR/boot-menu.patch"
+
+# extract initrd
+cd "$TMP_INITRD_DIR"
+cat "$TMP_DISC_DIR/initrd.gz" | gzip -d | cpio -i
+
+# add preseed.cfg and repack initrd
+cp "$SCRIPT_DIR/preseed.cfg" .
+find | cpio -o --format=newc | gzip -9c > "$TMP_DISC_DIR/initrd.gz"
+
+# build iso
+cd "$TMP_DISC_DIR"
+mkisofs -r -V "Ubuntu 16.10 Unattended" -cache-inodes -J -l -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -o "$TARGET_ISO" ./
 
 # go back to initial directory
 cd "$CURRENT_DIR"
