41cc6f86474d95634e8f01f99e988506595c0cb7
Niklas Salmoukas <niklas@salmoukas.com>
2017-02-11 21:16:05 +0100

    simplify build-iso.sh, add password support

--- ubuntu/16.10/build-iso.sh
+++ ubuntu/16.10/build-iso.sh
@@ -2,69 +2,43 @@
 set -e
 
 # get parameters
-IMAGE_TYPE=${1:-"netboot"}
-SOURCE_ISO_URL=""
+ROOT_PASSWORD=${1:-"`pwgen -N1 -B`"}
+TARGET_ISO=${2:-"`pwd`/ubuntu-16.10-netboot-amd64-unattended-$ROOT_PASSWORD.iso"}
+SOURCE_ISO_URL="http://archive.ubuntu.com/ubuntu/dists/yakkety/main/installer-amd64/current/images/netboot/mini.iso"
 
-case "$IMAGE_TYPE" in
-  netboot)
-    SOURCE_ISO_URL="http://archive.ubuntu.com/ubuntu/dists/yakkety/main/installer-amd64/current/images/netboot/mini.iso"
-    ;;
-  server)
-    SOURCE_ISO_URL="http://releases.ubuntu.com/16.10/ubuntu-16.10-server-amd64.iso"
-    ;;
-  *)
-    echo "Usage: $0 <image-type> <target-iso>"
-    echo "Note: valid image types are 'netboot' (default) and 'server'"
-    exit 1
-esac
+# encrypt root password
+ROOT_PASSWORD_ENCRYPTED="`printf "$ROOT_PASSWORD" | mkpasswd -s -m sha-512`"
 
-TARGET_ISO=${2:-"`pwd`/ubuntu-16.10-$IMAGE_TYPE-amd64-unattended.iso"}
-
-# get all directories
+# get directories
 CURRENT_DIR="`pwd`"
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 TMP_DOWNLOAD_DIR="`mktemp -d`"
 TMP_DISC_DIR="`mktemp -d`"
 TMP_INITRD_DIR="`mktemp -d`"
 
-# download and extract source iso
-wget "$SOURCE_ISO_URL" -O "$TMP_DOWNLOAD_DIR/source.iso"
-7z x "$TMP_DOWNLOAD_DIR/source.iso" "-o$TMP_DISC_DIR"
+# download and extract netboot iso
+wget "$SOURCE_ISO_URL" -O "$TMP_DOWNLOAD_DIR/netboot.iso"
+7z x "$TMP_DOWNLOAD_DIR/netboot.iso" "-o$TMP_DISC_DIR"
 
-# handle netboot image
-if [ "$IMAGE_TYPE" = "netboot" ]
-then
-  # patch boot menu
-  cd "$TMP_DISC_DIR"
-  patch -p1 -i "$SCRIPT_DIR/boot-menu.netboot.patch"
+# patch boot menu
+cd "$TMP_DISC_DIR"
+patch -p1 -i "$SCRIPT_DIR/boot-menu.patch"
 
-  # append preseed.cfg to initrd
-  cd "$SCRIPT_DIR"
-  cat "$TMP_DISC_DIR/initrd.gz" | gzip -d > "$TMP_INITRD_DIR/initrd"
-  echo "preseed.cfg" | cpio -o -H newc -A -F "$TMP_INITRD_DIR/initrd"
-  cat "$TMP_INITRD_DIR/initrd" | gzip -9c > "$TMP_DISC_DIR/initrd.gz"
+# prepare preseed.cfg
+ROOT_PASSWORD_ENCRYPTED_SAFE=$(printf '%s\n' "$ROOT_PASSWORD_ENCRYPTED" | sed 's/[[\.*/]/\\&/g; s/$$/\\&/; s/^^/\\&/')
+cp "$SCRIPT_DIR/preseed.cfg" "$TMP_INITRD_DIR/preseed.cfg"
+sed -i "s/d-i passwd\\/root-password-crypted password.*/d-i passwd\\/root-password-crypted password $ROOT_PASSWORD_ENCRYPTED_SAFE/g" "$TMP_INITRD_DIR/preseed.cfg"
 
-  # set isolinux prefix
-  ISOLINUX_PREFIX=""
-
-# handle server image
-elif [ "$IMAGE_TYPE" = "server" ]
-then
-  # patch boot menu
-  cd "$TMP_DISC_DIR"
-  patch -p1 -i "$SCRIPT_DIR/boot-menu.server.patch"
-
-  # add preseed file to iso
-  cp "$SCRIPT_DIR/preseed.cfg" "$TMP_DISC_DIR/preseed/unattended.seed"
-
-  # set isolinux prefix
-  ISOLINUX_PREFIX="isolinux/"
-fi
+# append preseed.cfg to initrd
+cd "$TMP_INITRD_DIR"
+cat "$TMP_DISC_DIR/initrd.gz" | gzip -d > "$TMP_INITRD_DIR/initrd"
+echo "preseed.cfg" | cpio -o -H newc -A -F "$TMP_INITRD_DIR/initrd"
+cat "$TMP_INITRD_DIR/initrd" | gzip -9c > "$TMP_DISC_DIR/initrd.gz"
 
 # build iso
 cd "$TMP_DISC_DIR"
 rm -r '[BOOT]'
-mkisofs -r -V "ubuntu 16.10 $IMAGE_TYPE unattended" -cache-inodes -J -l -b "${ISOLINUX_PREFIX}isolinux.bin" -c "${ISOLINUX_PREFIX}boot.cat" -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -o "$TARGET_ISO" ./
+mkisofs -r -V "ubuntu 16.10 netboot unattended" -cache-inodes -J -l -b isolinux.bin -c boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -input-charset utf-8 -o "$TARGET_ISO" ./
 
 # go back to initial directory
 cd "$CURRENT_DIR"
@@ -73,3 +47,6 @@ cd "$CURRENT_DIR"
 rm -r "$TMP_DOWNLOAD_DIR"
 rm -r "$TMP_DISC_DIR"
 rm -r "$TMP_INITRD_DIR"
+
+# done
+echo "The 'root' password is '$ROOT_PASSWORD'. Next steps: install system, login via root, change root password, deploy ansible (if applicable), enjoy!"
