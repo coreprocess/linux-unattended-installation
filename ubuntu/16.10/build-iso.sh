b771292dd9548f692418cc99a700531622dff0a0
Niklas Salmoukas <niklas@salmoukas.com>
2017-09-12 10:15:45 +0200

    upgrade procedure for 16.10, update readme

--- ubuntu/16.10/build-iso.sh
+++ ubuntu/16.10/build-iso.sh
@@ -2,12 +2,17 @@
 set -e
 
 # get parameters
-ROOT_PASSWORD=${1:-"`pwgen -N1 -B`"}
-TARGET_ISO=${2:-"`pwd`/ubuntu-16.10-netboot-amd64-unattended-$ROOT_PASSWORD.iso"}
-SOURCE_ISO_URL="http://archive.ubuntu.com/ubuntu/dists/yakkety/main/installer-amd64/current/images/netboot/mini.iso"
+SSH_PUBLIC_KEY_FILE=${1:-"$HOME/.ssh/id_rsa.pub"}
+TARGET_ISO=${2:-"`pwd`/ubuntu-16.10-netboot-amd64-unattended.iso"}
 
-# encrypt root password
-ROOT_PASSWORD_ENCRYPTED="`printf "$ROOT_PASSWORD" | mkpasswd -s -m sha-512`"
+# read ssh public key
+if [ ! -f "$SSH_PUBLIC_KEY_FILE" ];
+then
+    echo "Error: public SSH key $SSH_PUBLIC_KEY_FILE not found!"
+    exit 1
+fi
+
+SSH_PUBLIC_KEY="`cat "$SSH_PUBLIC_KEY_FILE"`"
 
 # get directories
 CURRENT_DIR="`pwd`"
@@ -17,7 +22,8 @@ TMP_DISC_DIR="`mktemp -d`"
 TMP_INITRD_DIR="`mktemp -d`"
 
 # download and extract netboot iso
-wget "$SOURCE_ISO_URL" -O "$TMP_DOWNLOAD_DIR/netboot.iso"
+SOURCE_ISO_URL="http://archive.ubuntu.com/ubuntu/dists/yakkety/main/installer-amd64/current/images/netboot/mini.iso"
+wget -4 "$SOURCE_ISO_URL" -O "$TMP_DOWNLOAD_DIR/netboot.iso"
 7z x "$TMP_DOWNLOAD_DIR/netboot.iso" "-o$TMP_DISC_DIR"
 
 # patch boot menu
@@ -25,9 +31,10 @@ cd "$TMP_DISC_DIR"
 patch -p1 -i "$SCRIPT_DIR/boot-menu.patch"
 
 # prepare preseed.cfg
-ROOT_PASSWORD_ENCRYPTED_SAFE=$(printf '%s\n' "$ROOT_PASSWORD_ENCRYPTED" | sed 's/[[\.*/]/\\&/g; s/$$/\\&/; s/^^/\\&/')
 cp "$SCRIPT_DIR/preseed.cfg" "$TMP_INITRD_DIR/preseed.cfg"
-sed -i "s/d-i passwd\\/root-password-crypted password.*/d-i passwd\\/root-password-crypted password $ROOT_PASSWORD_ENCRYPTED_SAFE/g" "$TMP_INITRD_DIR/preseed.cfg"
+# ... apply authorized keys
+SSH_PUBLIC_KEY_SAFE=$(printf '%s\n' "$SSH_PUBLIC_KEY" | sed 's/[[\.*/]/\\&/g; s/$$/\\&/; s/^^/\\&/')
+sed -i "s/###_SSH_AUTHORIZED_KEYS_###/$SSH_PUBLIC_KEY_SAFE/g" "$TMP_INITRD_DIR/preseed.cfg"
 
 # append preseed.cfg to initrd
 cd "$TMP_INITRD_DIR"
@@ -49,4 +56,4 @@ rm -r "$TMP_DISC_DIR"
 rm -r "$TMP_INITRD_DIR"
 
 # done
-echo "The 'root' password is '$ROOT_PASSWORD'. Next steps: install system, login via root, change root password, deploy via ansible (if applicable), enjoy!"
+echo "Next steps: install system, login via root, adjust the authorized keys, set a root password (if you want to), deploy via ansible (if applicable), enjoy!"
